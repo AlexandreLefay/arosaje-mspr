1448a7ac2fb67c9b2a1df021d5fb006fbf09cbf2
AlexandreLefay <alexandre.lefay@gmail.com>
2024-01-11 12:50:31 +0100

    init commit

---
+++ arosaje-backend/src/test/java/fr/epsi/mspr/arosaje/ArosajeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epsi.mspr.arosaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArosajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
