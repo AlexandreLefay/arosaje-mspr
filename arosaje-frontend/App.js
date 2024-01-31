b6dfd7a4a7ddedfd704e62f131e2380a70225409
huuuunny <doucet.yohann@gmail.com>
2024-01-31 09:46:43 +0100

    Ajout navbar

--- arosaje-frontend/App.js
+++ arosaje-frontend/App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Navbar from "./src/components/Navbar";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Navbar/>
       <Text>My react native app!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ arosaje-frontend/src/components/Navbar.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; // Assurez-vous d'avoir installé expo vector-icons
+
+const Navbar = () => {
+    return (
+        <View style={styles.container}>
+            {/* Section gauche: Photo de profil */}
+            <TouchableOpacity style={styles.profileContainer}>
+                <Image
+                    source={{ uri: 'https://sm.ign.com/t/ign_fr/cover/a/avatar-gen/avatar-generations_bssq.300.jpg' }} // Remplacez par l'URL de votre photo de profil
+                    style={styles.profilePic}
+                />
+            </TouchableOpacity>
+
+            {/* Section centrale: Nom de l'application */}
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>Arosaje</Text>
+            </View>
+
+            {/* Section droite: Icône de recherche */}
+            <TouchableOpacity style={styles.iconContainer}>
+                <Ionicons name="ios-search" size={24} color="black" />
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 10,
+        height: 60, // Hauteur de la navbar
+        backgroundColor: 'white', // Couleur de fond de la navbar
+        position: 'absolute', // Positionnement absolu par rapport à son conteneur
+        left: 0, // Aligné à gauche
+        right: 0, // Aligné à droite
+        bottom: 10, // Aligné en bas
+    },
+    profileContainer: {
+        flex: 1,
+        alignItems: 'flex-start',
+    },
+    profilePic: {
+        width: 40, // Ajustez selon votre besoin
+        height: 40, // Ajustez selon votre besoin
+        borderRadius: 20, // Cela rendra l'image circulaire
+    },
+    titleContainer: {
+        flex: 2,
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 18, // Ajustez selon votre besoin
+        fontWeight: 'bold',
+    },
+    iconContainer: {
+        flex: 1,
+        alignItems: 'flex-end',
+    },
+});
+
+export default Navbar;
